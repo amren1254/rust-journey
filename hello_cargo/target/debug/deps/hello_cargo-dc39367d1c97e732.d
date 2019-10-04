777bf252dbe8632da393c3b1358342f0fa3daff8
amren1254 <amren1254@gmail.com>
2019-10-05 01:20:11 +0530

    HEllo world using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-dc39367d1c97e732.d
@@ -0,0 +1,5 @@
+/home/drax/github/rust-journey/hello_cargo/target/debug/deps/hello_cargo-dc39367d1c97e732.rmeta: src/main.rs
+
+/home/drax/github/rust-journey/hello_cargo/target/debug/deps/hello_cargo-dc39367d1c97e732.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/drax/github/rust-journey/hello_cargo/target/debug/hello_cargo: /home/drax/github/rust-journey/hello_cargo/src/main.rs
